9099b6e3efc0f358698c1e3bd3a4a2d3117f6347
zhihun <zhihun@outlook.com>
2021-09-14 23:22:15 +0800

    hot1 commit

--- src/main/java/com/phome/yypp/YyppApplication.java
+++ src/main/java/com/phome/yypp/YyppApplication.java
@@ -13,6 +13,9 @@ public class YyppApplication {
     public static void main(String[] args) {
         SpringApplication.run(YyppApplication.class, args);
         System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
     }
 
 }
